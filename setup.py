b41a4bf35f6bffbe19d5d667d7eef3c0b9915fa0
Aly Merchant <aly.merchant@ec.gc.ca>
2014-11-10 08:21:22 -0500

    sample build

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="RAMP Configuration Service",
+    version="0.1.0",
+    author="Environment Canada",
+    author_email="mike.weech@ec.gc.ca",
+    url="https://github.com/RAMP-PCAR/RCS",
+    py_modules=['rcs.py'],
+)
