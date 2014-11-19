fd03625139cb890b61419802f8dfddb5d46572b6
Aly Merchant <aly.merchant@ec.gc.ca>
2014-11-19 10:33:00 -0500

    fix WMS getFeatureInfo types

--- regparse/wms.py
+++ regparse/wms.py
@@ -3,11 +3,11 @@ import requests
 def make_feature_info( data ):
     fi_type = data.get('feature_info_type',None) 
     if fi_type == 'text/plain':
-        return { 'mimeType':fi_type, 'parser':'stringParse.js' }
+        return { 'mimeType':fi_type, 'parser':'stringParse' }
     if fi_type == 'text/html':
-        return { 'mimeType':fi_type, 'parser':'stringParse.js' }
+        return { 'mimeType':fi_type, 'parser':'htmlRawParse' }
     if fi_type == 'application/json':
-        return { 'mimeType':fi_type, 'parser':'stringParse.js' }
+        return { 'mimeType':fi_type, 'parser':'jsonRawParse' }
     return None
 
 def make_node( data, id ):
