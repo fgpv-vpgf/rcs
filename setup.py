be0a911eec98c05d7fd497841a3a93088529a614
Aly Merchant <aly.merchant@ec.gc.ca>
2014-11-20 14:15:51 -0500

    packaging updates

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name="RAMP Configuration Service",
-    version="0.1.0",
+    name="rcs",
+    description="RAMP Configuration Service",
+    version="1.0.0",
     author="Environment Canada",
     author_email="mike.weech@ec.gc.ca",
     url="https://github.com/RAMP-PCAR/RCS",
-    py_modules=['rcs.py'],
+    py_modules=['rcs','wfastcgi'],
+    packages=['db','regparse'],
 )
