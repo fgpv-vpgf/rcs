a224711e1ced1f20ed305370faf43e88758a9f21
Aly Merchant <aly.merchant@ec.gc.ca>
2014-11-28 16:29:34 -0500

    Working logger for all exceptions; but triggers the log twice

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
-DB_USER='rcs'
-DB_PASS='rcs'
-DB_NAME='rcs'
-DB_HOST='localhost'
-DB_PORT=27017
-DB_CONN='http://rcs:rcs@localhost:5984/'
+DB_NAME='rcs_cache'
+DB_CONN='http://localhost:5984/'
+
+LOGFILE='rcs.log'
+# https://docs.python.org/2/library/logging.html#levels
+# common options DEBUG 10; ERROR 40
+LOGLEVEL=10
 
 REG_SCHEMA='rcs_schema_v1.json'
