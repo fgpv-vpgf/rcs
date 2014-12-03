c96e95fbc6b0973542ad15132fbae037500dfb42
Aly Merchant <aly.merchant@ec.gc.ca>
2014-12-03 09:35:34 -0500

    bump version to 1.5.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name="rcs",
     description="RAMP Configuration Service",
-    version="1.5.0",
+    version="1.5.1",
     author="Environment Canada",
     author_email="mike.weech@ec.gc.ca",
     url="https://github.com/RAMP-PCAR/RCS",
