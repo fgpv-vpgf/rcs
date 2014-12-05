282e023e775ba613fd0654654343b367cdc694cd
Aly Merchant <aly.merchant@ec.gc.ca>
2014-12-05 12:14:15 -0500

    add a wrapper function for checking signatures

--- regparse/sigcheck.py
+++ regparse/sigcheck.py
@@ -2,9 +2,14 @@
 Tests RCS request signatures for validity
 """
 from __future__ import division, print_function, unicode_literals
-import hashlib, hmac, base64, logging, flask
+
+import hashlib, hmac, base64, logging, flask, iso8601, datetime
+
+from flask.ext.restful import abort
+from functools import wraps
 
 def get_logger():
+    """This function does not deserve a comment"""
     return flask.current_app.logger
 
 def sign( key, *parts ):
@@ -17,34 +22,75 @@ def sign( key, *parts ):
     """
     logger = get_logger()
     msg = ''.join( parts )
-    logger.info( msg )
+    logger.debug( msg )
     h = hmac.new( str(key), msg, digestmod=hashlib.sha256 )
-    return base64.urlsafe_b64encode( h.digest() )
+    return base64.urlsafe_b64encode( h.digest() ).replace('=','')
 
 def test_request( request ):
     """
+    Test the signature of a given request.
+
+    Pulls the signature components from the request and generates a reference signature.
+    Tests if the reference signature matches the Authorization header in the request.
+
+    :param request: A flask request object containing the request to validate
+    :returns: bool -- if the generated signature matches the authorization header
     """
     logger = get_logger()
     for h in 'Authorization TimeStamp Sender'.split():
-        logger.info( h+': '+request.headers.get(h) )
+        logger.debug( h+': '+request.headers.get(h) )
     dt = request.headers.get( 'TimeStamp' )
     psk = 'test_-k'
     cid = request.headers.get( 'Sender' )
     rqpath = request.path
     rqbody = request.data
 
-    logger.info( sign( psk, rqpath, cid, dt, rqbody ) )
-    return False
+    sig = sign( psk, rqpath, cid, dt, rqbody )
+    logger.info( 'Signature received: {0}  ##  Signature generated: {1}'.format(request.headers.get('Authorization'),sig) )
+    return sig == request.headers.get('Authorization')
 
-def check_time( sent ):
+def validate(func):
+    """
+    Wraps a service endpoint and checks the authentication headers.
+
+    Tests the signature on the request and ensures the request time is current.
+    Failure results in a 401 HTTP error if SIG_CHECK is enabled, if not validation
+    will always pass.
+    """
+    def validation_fail( msg ):
+        if flask.current_app.config['SIG_CHECK']:
+            abort(401,msg=msg)
+        else:
+            # be vocal if SIG_CHECK is accidentally set to False in production
+            get_logger().warning('Authorization failure [{0}] ignored: SIG_CHECK is off in the config'.format(msg))
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        if not test_request( flask.request ):
+            validation_fail( 'Invalid signature' )
+        try:
+            if not check_time( flask.request ):
+                validation_fail( 'Request time out of sync' )
+        except iso8601.ParseError:
+            abort(400,msg="Unparsable timestamp")
+        return func(*args, **kwargs)
+    return decorated_function
+
+def check_time( request ):
     """
     Ensure the sent time is within 2 minutes of the current time
 
     :param sent: An ISO8601 encoded date
     :type sent: str
+    :raises: ParseError -- if the datetime cannot be parsed
     :returns: bool -- sent time is within 2 minutes of the current time
     """
-    return False
+    logger = get_logger()
+    sent = request.headers.get( 'TimeStamp' )
+    dt = iso8601.parse_date( sent )
+    now = datetime.datetime.now( iso8601.iso8601.Utc() )
+    two_min = datetime.timedelta(minutes=2)
+    logger.info( 'Header date: {0}  ##  Current timestamp: {1}'.format(sent,now) )
+    return -two_min < now-dt < two_min
 
 if __name__ == '__main__':
     dt = '2007-01-25T12:00:00Z'
