3aa7f73520d04fb827fde6f8ecf703325709f062
Aly Merchant <aly.merchant@ec.gc.ca>
2014-12-10 16:03:36 -0500

    new config parameters; new schema version

--- config.py
+++ config.py
@@ -3,10 +3,11 @@ AUTH_DB='rcs_auth'
 DB_CONN='http://localhost:5984/'
 
 SIG_CHECK=True
+METADATA_URL='http://www2.dmsolutions.ca/cgi-bin/mswfs_gmap?version=1.3.0&request=getcapabilities&service='
 
 LOG_FILE='rcs.log'
 # https://docs.python.org/2/library/logging.html#levels
 # common options DEBUG 10; ERROR 40
 LOG_LEVEL=10
 
-REG_SCHEMA='rcs_schema_v1.json'
+REG_SCHEMA='schemas/rcs_reg_schema_v1_1_0.json'
