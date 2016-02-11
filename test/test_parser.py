f84167315e0f567e10b7c1c2c55ae08da51d6722
Aly Merchant <aly.merchant@ec.gc.ca>
2016-02-11 15:41:34 -0500

    style(test): flake8 fixes

--- test/test_parser.py
+++ test/test_parser.py
@@ -1,20 +1,23 @@
 import pytest, regparse.metadata as md
 
-config_1 = { 'METADATA_URL': 'http://www2.dmsolutions.ca/cgi-bin/mswfs_gmap?version=1.3.0&request=getcapabilities&service={0}', 'CATALOGUE_URL': 'http://www.google.ca/' }
-config_2 = { 'METADATA_URL': 'http://nowhere.example.com/{0}', 'CATALOGUE_URL': "{0}" }
-valid_url = { 'metadata': { 'metadata_url': 'http://www2.dmsolutions.ca/cgi-bin/mswfs_gmap?version=1.3.0&request=getcapabilities&service=wms', 'catalogue_url': 'http://www.google.ca/' } }
-valid_uuid = { 'metadata': { 'uuid': 'wms' } }
+config_1 = {'METADATA_URL': 'http://www2.dmsolutions.ca/cgi-bin/mswfs_gmap?version=1.3.0&request=getcapabilities&service={0}', 'CATALOGUE_URL': 'http://www.google.ca/'}  # NOQA
+config_2 = {'METADATA_URL': 'http://nowhere.example.com/{0}', 'CATALOGUE_URL': "{0}"}
+valid_url = {'metadata': {'metadata_url': 'http://www2.dmsolutions.ca/cgi-bin/mswfs_gmap?version=1.3.0&request=getcapabilities&service=wms', 'catalogue_url': 'http://www.google.ca/'}}  # NOQA
+valid_uuid = {'metadata': {'uuid': 'wms'}}
+
 
 @pytest.mark.xfail
 def test_url():
-    u,c = md.get_url( valid_url, config_1 )
+    u, c = md.get_url(valid_url, config_1)
     assert u == valid_url['metadata']['metadata_url']
+
 
 @pytest.mark.xfail
 def test_uuid():
-    u,c = md.get_url( valid_uuid, config_1 )
+    u, c = md.get_url(valid_uuid, config_1)
     assert u == valid_url['metadata']['metadata_url']
 
+
 def test_bad_config():
-    with pytest.raises( md.MetadataException ):
-        md.get_url( valid_uuid, config_2 )
+    with pytest.raises(md.MetadataException):
+        md.get_url(valid_uuid, config_2)
