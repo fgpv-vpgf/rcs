3c127943961de1f8f9d0bea0943b407c262944da
Aly Merchant <aly.merchant@ec.gc.ca>
2016-03-04 18:25:33 -0500

    chore(build): add missing packages

--- setup.py
+++ setup.py
@@ -12,6 +12,6 @@ setup(
     author_email="mike.weech@ec.gc.ca",
     url="https://github.com/fgpv-vpgf/rcs",
     py_modules=['run', 'wfastcgi', 'config', 'seed_qa_keys'],
-    packages=['services'],
+    packages=['services', 'services.db', 'services.regparse'],
     license='MIT',
 )
