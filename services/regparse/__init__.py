1a816ae2a0c41109f7d10f56c37a9f2f2d4153db
Aly Merchant <aly.merchant@ec.gc.ca>
2016-03-15 15:17:29 -0400

    fix(regparse): rename dependencies

--- services/regparse/__init__.py
+++ services/regparse/__init__.py
@@ -1,4 +1,4 @@
-import wms, esri_feature, sigcheck
+import ogc, esri, sigcheck
 from universal import ServiceTypes, ServiceEndpointException, make_node, make_id
-__all__ = ['esri_feature', 'sigcheck', 'wms', 'make_id',
+__all__ = ['esri', 'sigcheck', 'ogc', 'make_id',
            'ServiceTypes', 'ServiceEndpointException', 'make_node']
