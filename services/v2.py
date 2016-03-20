d5c0b6a2e126bb6b81241154566dcc6f0fa0ecd0
Aly Merchant <aly.merchant@ec.gc.ca>
2016-03-20 18:24:14 -0400

    feat(debug): add debugging endpoints
    relates #10

--- services/v2.py
+++ services/v2.py
@@ -1,11 +1,12 @@
 from registration import Register, Refresh
 from retrieval import DocV2, DocsV2
+from debug import AccessLog, Log
 
 from flask import Blueprint
 from flask.ext.restful import Api
 
 
-def make_blueprint():
+def make_blueprint(app):
     """
     Create a v2 Flask Blueprint
     """
@@ -15,4 +16,8 @@ def make_blueprint():
     api.add_resource(Refresh, '/refresh/<string:arg>', '/refresh/<string:arg>/<int:limit>')
     api.add_resource(DocV2, '/doc/<string:lang>/<string:smallkey>')
     api.add_resource(DocsV2, '/docs/<string:lang>/<string:smallkeylist>')
+    if app.config.get('DEBUG_ENDPOINTS'):
+        api.add_resource(AccessLog, '/accesslog', '/accesslog/<int:index>')
+        api.add_resource(Log, '/log', '/log/<int:index>')
+
     return bp
