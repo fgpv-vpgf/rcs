ea86ee0823665870c4a7c514cf30feda3a14a496
Aly Merchant <aly.merchant@ec.gc.ca>
2016-03-20 18:24:14 -0400

    feat(debug): add debugging endpoints
    relates #10

---
+++ services/debug.py
@@ -0,0 +1,28 @@
+from flask import current_app, Response
+from flask.ext.restful import request, abort, Resource
+
+
+class FetchFile(Resource):
+
+    def get(self, index=None):
+        if self.file_param not in current_app.config:
+            return 'Parameter {} is not enabled'.format(self.file_param), 404
+        lookup_file = current_app.config[self.file_param]
+        if index:
+            lookup_file += '.{}'.format(index)
+        with open(lookup_file) as logfile:
+            return Response(logfile.read(), status=200, mimetype='text/plain')
+
+
+class AccessLog(FetchFile):
+
+    def __init__(self):
+        super(AccessLog, self).__init__()
+        self.file_param = 'ACCESS_LOG'
+
+
+class Log(FetchFile):
+
+    def __init__(self):
+        super(Log, self).__init__()
+        self.file_param = 'LOG_FILE'
