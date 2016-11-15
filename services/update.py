ac37dbb89365640a4d6bbf3877edc8c71085e545
Dan Bowerman <dan.bowerman@ec.gc.ca>
2016-11-15 11:17:47 -0500

    refactor(update): Refactored v2 Update endpoint
    RCS v2 now supports the ability to update specific fields in a previous registration, without re-registering wholesale. WMS registrations will also store whether a layer is queryable or not. The refactor also resolves an issue where feature_info_format in WMS layers was not being set.
    
    Closes #59, #60

--- services/update.py
+++ services/update.py
@@ -29,30 +29,22 @@ class Update(Resource):
             return Response(json.dumps(msg), mimetype='application/json', status=500)
 
         def replace_if_set(params):
-            try:
-                for p in params:
+            for p in params:
+                if p in payload_seg:
                     request_seg[p] = payload_seg[p]
-            except:
-                print p + " not found in request"
 
         try:
             payload = json.loads(request.data)
             for lang in langs:
                 request_seg = dbdata['request'][lang]
                 payload_seg = payload[lang]
-                replace_if_set(['service_url'])
-                replace_if_set(['service_name'])
-                replace_if_set(['metadata'])
+                replace_if_set(['service_url', 'service_name', 'metadata'])
                 if payload_seg['service_type'] in ['esriFeature', 'esriImage', 'esriTile']:
                     replace_if_set(['display_field'])
                 elif payload_seg['service_type'] == 'esriMapServer':
-                    replace_if_set(['scrape_only'])
-                    replace_if_set(['recursive'])
+                    replace_if_set(['scrape_only', 'recursive'])
                 elif payload_seg['service_type'] == 'ogcWms':
-                    replace_if_set(['scrape_only'])
-                    replace_if_set(['recursive'])
-                    replace_if_set(['legend_format'])
-                    replace_if_set(['feature_info_format'])
+                    replace_if_set(['scrape_only', 'recursive', 'legend_format', 'feature_info_format'])
                 v2_node, v1_node = regparse.make_node(key, dbdata["request"], current_app.config)
                 db.put_doc(key, payload[lang]["service_type"], dbdata["request"],
                            layer_config=v2_node, v1_config=v1_node)
